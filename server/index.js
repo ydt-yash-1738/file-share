103841a5b36db293e0626184d04d79fe09b8cd9e
yoda <98830917+ydt-yash-1738@users.noreply.github.com>
2024-01-31 21:38:57 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,6 +17,9 @@ app.use(cors(
 ));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.get("/", (req, res) => {
+    res.json("Hello!!");
+})
 app.use('/', router);
 const PORT = process.env.PORT || 8000;
 
