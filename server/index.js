1e5f786129d37832f1a669ab155e3f58dc2ff709
yoda <98830917+ydt-yash-1738@users.noreply.github.com>
2024-01-31 22:11:54 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,13 +8,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors(
-  {
-    origin: ["https://ydt-file-share-private.vercel.app"],
-    methods: ["POST", "GET"],
-    credentials: true
-  }
-));
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.get("/", (req, res) => {
